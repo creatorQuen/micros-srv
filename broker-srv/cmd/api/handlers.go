07e53cfdb9308e1ed55a4abf6bb40edf62578d36
creatorQuen <spastime@yandex.ru>
2023-08-02 00:28:59 +0300

    feat: Add helpers for broker service.

--- broker-srv/cmd/api/handlers.go
+++ broker-srv/cmd/api/handlers.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
-func (a *Config) Broker(w http.ResponseWriter, r *http.Request) {
+func (c *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	body := jsonResponse{
 		Error:   false,
-		Message: "Puchnch broker",
+		Message: "Throght broker",
 	}
 
-	out, _ := json.MarshalIndent(body, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = c.writeJSON(w, http.StatusOK, body)
 }
